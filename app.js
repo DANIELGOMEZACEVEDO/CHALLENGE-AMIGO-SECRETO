df2e416f24265cba87c2baee39748c2b31e5fa8f
Daniel Fernando Gomez <danielfernandogomezacevedo@gmail.com>
2025-02-19 13:13:39 -0500

    inicio challenge

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function agregarAmigos(){
+    let amigos = document.querySelector("input").value;
+    cajitaAmigos.push(amigos);
+    console.log(cajitaAmigos);
+    return 
+}
+
+
+function guardar_nombre(){
+
+    let listaAmigos = document.querySelector('h1').value;
+
+    if (agregarAmigos.call){
+
+        cajitaAmigos in listaAmigos
+
+    }else{
+        alert("no sirves")
+    }
+}
+let cajitaAmigos = [];
+agregarAmigos()
+guardar_nombre
+
+
+function limpiar(){
+
+}
