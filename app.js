4cd2b9ead939331de7d5bac65707613da6b065ee
Daniel Fernando Gomez <danielfernandogomezacevedo@gmail.com>
2025-03-15 22:52:38 -0500

    CORRECCIONES Y TERMINACION DEL AMIGO SECRETO

--- app.js
+++ app.js
@@ -1,30 +1,59 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-function agregarAmigos(){
-    let amigos = document.querySelector("input").value;
-    cajitaAmigos.push(amigos);
-    console.log(cajitaAmigos);
-    return 
-}
+// creacion del array
+let amigos = [];
 
 
-function guardar_nombre(){
+// funcion que agrega amigos a una lista
+function agregarAmigos() {
+    console.log("boton presionado")
+    const input = document.getElementById('amigos');
+    const nombre = input.value.trim();
 
-    let listaAmigos = document.querySelector('h1').value;
-
-    if (agregarAmigos.call){
-
-        cajitaAmigos in listaAmigos
-
-    }else{
-        alert("no sirves")
+    // corroborar que no se añada un valor vacio
+    if(nombre === '') {
+        alert('Por favor ingresa un nombre valido.')
+        return;
     }
-}
-let cajitaAmigos = [];
-agregarAmigos()
-guardar_nombre
 
-
-function limpiar(){
+    amigos.push(nombre);
+    actualizarlista();
+    input.value = '';
 
 }
+
+// funcion que actualiza los nombres en el html
+function actualizarlista() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    amigos.forEach((nombre,index) => {
+        const item = document.createElement('li');
+        item.textContent = nombre;
+        lista.appendChild(item);
+
+    })
+}
+
+// funcion que selecciona un amigo al azar
+function sortearAmigo() {
+    if(amigos.length === 0) {
+        alert('la lista esta vacia. agregue nombres para realizar el sorteo. ');
+        return;
+    }
+
+    const aleatorio = Math.floor(Math.random()* amigos.length);
+    const seleccion_amigo = amigos[aleatorio];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Tu amigo secreto es: ${seleccion_amigo}`;
+    
+}
+
+//funcion que reinicia el juego poniendo todo a valores iniciales
+    function reiniciar() {
+
+        amigos = [];
+        actualizarlista();
+        document.getElementById('resultado').innerHTML = '';
+
+    }
